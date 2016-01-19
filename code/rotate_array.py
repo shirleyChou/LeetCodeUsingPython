8201312d4a8e0c4cca4199f200cf131cd0870c42
zhouyi <zhouyi@ibuka.cn>
2016-01-19 15:31:27 +0800

    add rotate_array.py

---
+++ code/rotate_array.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+# https://leetcode.com/problems/rotate-array/
+# Difficulty: Easy
+# Tags: Array
+
+"""
+Rotate an array of n elements to the right by k steps.
+
+For example, with n = 7 and k = 3, the array [1,2,3,4,5,6,7] is rotated to [5,6,7,1,2,3,4].
+
+Note:
+Try to come up as many solutions as you can, there are at least 3 different ways to solve this problem.
+"""
+
+# method one
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        for item in xrange(k):
+            nums.insert(0, nums.pop())
+
+# method two
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        k = k % len(nums)
+        nums[:] = nums[-k:] + nums[:-k]
